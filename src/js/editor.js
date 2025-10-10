d2915d45fa1b08d958f0d25d3b133465c8eeeb09
n-babillon <nelly.babillon@gmail.com>
2025-10-10 13:05:46 +0200

    Update theme and block patterns with new image URLs and styles; adjust font sizes and layout for better responsiveness.

--- src/js/editor.js
+++ src/js/editor.js
@@ -6,3 +6,5 @@ import "./editor/hot-reload";
 import "./editor/button";
 import "./editor/images-text";
 import "./editor/cards-row";
+import "./modules/parallax-group-extension";
+import "./modules/acf-block-extension";
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
 // Import de tous les scripts JavaScript du thème
 import './modules/taulignan.js';
 import './modules/navigation.js';
-import './modules/parallax-group-extension.js';
-import './modules/customizer.js';
-import './modules/acf-block-extension.js';
 import './modules/custom-swiper.js';
